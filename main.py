5de1cf873b866b29afd04422e13d02b56058ce7a
zihadev <34001897+zihadev@users.noreply.github.com>
2024-07-23 08:37:33 +0200

    links added

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ Below you can find some of the apps I have built in Python. Feel free to contact
 st.success(content2)
 
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5,0.25,1.5])
 df = pd.read_csv("data.csv", sep=";")
 
 
@@ -55,9 +55,15 @@ with col3:
     for index, row in df.iterrows():
         if index % 2 == 0:  # Check if the index is even
             st.header(row['title'])
-        # st.write(index, row)
+            st.write(row['description'])
+            st.image('images/'+row["image"])
+            st.write(f"[Source Code]({row['url']})")
+
 
 with col4:
     for index, row in df.iterrows():
         if index % 2 != 0:  # Check if the index is even
             st.header(row['title'])
+            st.write(row['description'])
+            st.image('images/' + row["image"])
+            st.write(f"[Source Code]({row['url']})")
